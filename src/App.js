95879ba1f72fbcae2476544b68dde3dceb6addef
mr.mewadi <kamleshpurbiya2004@gmail.com>
2025-06-21 08:17:39 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { Link, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -11,34 +12,36 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <div>
-      <Navbar expand="lg" className='fixed-top bg-body-tertiary shadow'>
-        <Container>
-          <Navbar.Brand>
-            <Link to="/" className='navbar-brand text-success fw-semibold'>
-              React Restaurant
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='me-auto justify-content-end w-100'>
-              <Nav.Link href='/' className='active text-uppercase'>Home</Nav.Link>
-              <Nav.Link href='/menu' className='text-uppercase'>Menu</Nav.Link>
-              <Nav.Link href='/about' className='text-uppercase'>About</Nav.Link>
-              <Nav.Link href='/contact' className='text-uppercase'>Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Router>
+        <Navbar expand="lg" className='fixed-top bg-body-tertiary shadow'>
+          <Container>
+            <Navbar.Brand>
+              <Link to="/" className='navbar-brand text-success fw-semibold'>
+                React Restaurant
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto justify-content-end w-100'>
+                <Nav.Link href='/' className='active text-uppercase'>Home</Nav.Link>
+                <Nav.Link href='/menu' className='text-uppercase'>Menu</Nav.Link>
+                <Nav.Link href='/about' className='text-uppercase'>About</Nav.Link>
+                <Nav.Link href='/contact' className='text-uppercase'>Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/menu' element={<Menu />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
 
       <footer className='bg-body-tertiary'>
-        <p className='p-3 m-0 text-center'>copyright @ made by Ionut Cora</p>
+        <p className='p-3 m-0 text-center'>copyright @ made by Mr.mewadii</p>
       </footer>
     </div>
   );
